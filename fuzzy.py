cfe0ea8e0b748253f207b860753d018e3c0d3bf5
Jeremy Wong <navhaxs@hotmail.com>
2016-10-11 15:42:16 +1100

    added controller

--- fuzzy.py
+++ fuzzy.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import socket
 import time
 import traceback
 from kitty.model import *
 from kitty.targets.server import ServerTarget
+from kitty.fuzzers import ServerFuzzer
 from kitty.controllers.base import BaseController
+from katnip.controllers.client.process import ClientProcessController
+from kitty.interfaces import WebInterface
+
 
 ####################################
 # About this file
 ####################################
-# There are three broad parts to the fuzzer, written using the Kitty framework:
+# There are three broad parts to this kitty-based fuzzer:
 #
 # 1) Data model
 # 2) Target
@@ -21,18 +25,12 @@ from kitty.controllers.base import BaseController
 ################# Data Model #################
 
 http_get_request_template = Template(name='HTTP_GET_V3', fields=[
-    String('GET', name='method', fuzzable=False),   # 1. Method - a string with the value "GET"
-    Delimiter(' ', name='space1', fuzzable=False),  # 1.a The space between Method and Path
-    String('/index.html', name='path'),             # 2. Path - a string with the value "/index.html"
-    Delimiter(' ', name='space2'),                  # 2.a. The space between Path and Protocol
-    String('HTTP', name='protocol name'),           # 3.a Protocol Name - a string with the value "HTTP"
-    Delimiter('/', name='fws1'),                    # 3.b The '/' after "HTTP"
-    Dword(1, name='major version',                  # 3.c Major Version - a number with the value 1
-          encoder=ENC_INT_DEC),                      # encode the major version as decimal number
-    Delimiter('.', name='dot1'),                    # 3.d The '.' between 1 and 1
-    Dword(1, name='major version',                  # 3.e Minor Version - a number with the value 1
-          encoder=ENC_INT_DEC),                      # encode the minor version as decimal number
-    Static('\r\n\r\n', name='eom')                  # 4. The double "new lines" ("\r\n\r\n") at the end of the request
+    String('GET', name='method'),           # 1. Method - a string with the value "GET"
+    Delimiter(' ', name='space1'),          # 1.a The space between Method and Path
+    String('/index.html', name='path'),     # 2. Path - a string with the value "/index.html"
+    Delimiter(' ', name='space2'),          # 2.a. The space between Path and Protocol
+    String('HTTP/1.1', name='protocol'),    # 3. Protocol - a string with the value "HTTP/1.1"
+    Delimiter('\r\n\r\n', name='eom'),      # 4. The double "new lines" ("\r\n\r\n") at the end of the http request
 ])
 
 ################# Target #################
@@ -126,21 +124,82 @@ class LocalProcessController(BaseController):
         self._process_args = process_args
         self._process = None
 
-################# Actual fuzzer code #################
-target = TcpTarget('Example Target')
+
+    def pre_test(self, test_num):
+        '''start the victim'''
+        ## stop the process if it still runs for some reason
+        if self._process:
+            self._stop_process()
+        cmd = [self._process_path] + self._process_args
+        ## start the process
+        self._process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        ## add process information to the report
+        self.report.add('process_name', self._process_name)
+        self.report.add('process_path', self._process_path)
+        self.report.add('process_args', self._process_args)
+        self.report.add('process_id', self._process.pid)
+
+
+    def post_test(self):
+        '''Called when test is done'''
+        self._stop_process()
+        ## Make sure process started by us
+        assert(self._process)
+        ## add process information to the report
+        self.report.add('stdout', self._process.stdout.read())
+        self.report.add('stderr', self._process.stderr.read())
+        self.logger.debug('return code: %d', self._process.returncode)
+        self.report.add('return_code', self._process.returncode)
+        ## if the process crashed, we will have a different return code
+        self.report.add('failed', self._process.returncode != 0)
+        self._process = None
+        ## call the super
+        super(ClientProcessController, self).post_test()
+
+    def teardown(self):
+        '''
+        Called at the end of the fuzzing session, override with victim teardown
+        '''
+        self._stop_process()
+        self._process = None
+        super(ClientProcessController, self).teardown()
+
+    def _stop_process(self):
+        if self._is_victim_alive():
+            self._process.terminate()
+            time.sleep(0.5)
+            if self._is_victim_alive():
+                self._process.kill()
+                time.sleep(0.5)
+                if self._is_victim_alive():
+                    raise Exception('Failed to kill client process')
+
+    def _is_victim_alive(self):
+        return self._process and (self._process.poll() is None)
+
+################# Actual fuzzer runner code #################
+target = TcpTarget('Example Target', 'localhost', 8080)
 
 # Define controller
-controller = LocalProcessController()
+#controller = LocalProcessController('Example Controller', 'lswsctrl', '/home/jz/root/bin/', '')
+env = os.environ.copy()
+controller = ClientProcessController(
+        'ServerController',
+        '/home/jz/root/bin/lswsctrl',
+        ['http://localhost:8082/fuzzed'],
+        process_env=env
+    )
+
 target.set_controller(controller)
 
 # Define model
 model = GraphModel()
-model.connect(http_get_request_template) 
+model.connect(http_get_request_template)
 
 # Define fuzzer
 fuzzer = ServerFuzzer()
 fuzzer.set_model(model)
 fuzzer.set_target(target)
-fuzzer.set_interface(WebInterface(port=web_port))
+fuzzer.set_interface(WebInterface(host='0.0.0.0', port=26000))
 #fuzzer.set_delay_between_tests(0.2)
 fuzzer.start()
